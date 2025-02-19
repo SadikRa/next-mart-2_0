6e51f6efe09f3d365cea6bf23b4d87c9090868ee
SadikRa <sadikrahman494@gmail.com>
2025-02-19 23:28:24 +0600

    today done

--- src/components/modules/home/Category/index.tsx
+++ src/components/modules/home/Category/index.tsx
@@ -6,23 +6,28 @@ import Link from "next/link";
 
 const Category = async () => {
   const { data: categories } = await getAllCategories();
+
   return (
-    <div className="container mx-auto my-20">
-      <div className="flex items-center justify-between">
-        <h2 className="font-bold text-2xl">Category</h2>
+    <div className="container mx-auto my-16 px-4">
+      {/* Header */}
+      <div className="flex flex-col sm:flex-row items-center justify-between gap-4 sm:gap-0">
+        <h2 className="font-bold text-2xl text-center sm:text-left">
+          Category
+        </h2>
         <Link href={"/products"}>
           <Button variant="outline" className="rounded-full">
-            {" "}
             View All
           </Button>
         </Link>
       </div>
-      <div className="grid grid-cols-6 gap-4">
-        {Array(12).fill(categories?.[0]).map((category: ICategory, idx: number) => (
-          <>
+
+      {/* Categories Grid */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4 mt-8">
+        {Array(12)
+          .fill(categories?.[0])
+          .map((category: ICategory, idx: number) => (
             <CategoryCard key={idx} category={category} />
-          </>
-        ))}
+          ))}
       </div>
     </div>
   );
